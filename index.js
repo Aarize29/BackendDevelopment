f384dc5ff9fb7d6114a2fc3b8d22a0c1200574bf
Aarize29 <1ds21ai045@dsce.edu.in>
2023-04-10 22:56:18 +0530

    express js

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-import http from 'http'
-import  gfName,{generateRandom} from './function.js'
-import fs from 'fs'
-//console.log(http)
+// import http from 'http'
+// import  gfName,{generateRandom} from './function.js'
+// import fs from 'fs'
 
-// http.createServer((req,res)=>{
-//     res.end('Hello World')
-// }
-// ).listen(3000,()=>{
-//     console.log('Server is running on port 3000')
-// }
-// )
+// import path from 'path'
+// //console.log(http)
 
-console.log(generateRandom())
-const server=http.createServer((req,res)=>{
-    if(req.url==='/'){
-        res.end(`<h1> The rondom number is ${generateRandom()} </h1>`)
-    }
-    else if(req.url==='/about'){
-        res.end(fs.readFileSync('index.html'))
-    }
-    else{
-        res.end(`${gfName} is not here`)
-    }    
+// // http.createServer((req,res)=>{
+// //     res.end('Hello World')
+// // }
+// // ).listen(3000,()=>{
+// //     console.log('Server is running on port 3000')
+// // }
+// // )
+
+// console.log(generateRandom())
+
+// const server=http.createServer((req,res)=>{
+//     if(req.url==='/'){
+//         res.end(`<h1> The rondom number is ${generateRandom()} </h1>`)
+//     }
+//     else if(req.url==='/about'){
+//         res.end(fs.readFileSync('index.html'))
+//     }
+//     else{
+//         res.end(`${gfName} is not here`)
+//     }    
     
+// })
+
+// server.listen(3000,()=>{
+//     console.log('Server is running on port 3000')
+// })
+
+
+
+import express from "express"
+import fs from "fs"
+import path from "path"
+const app=express()
+
+app.get('/',(req,res)=>{
+   const pathloc=path.resolve()
+
+   res.sendFile(path.join(pathloc,'index.html'))
+})
+app.get('/about',(req,res)=>{
+    res.send("About Page")
 })
 
-server.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+app.listen(5000,()=>{
+    console.log("Server is listening ")
 })
